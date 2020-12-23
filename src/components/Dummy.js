bfa5ef6e0225ad4056b5f4bed6bf32f93358a2cd
sana mahamid <sanaa_6-5.7@hotmail.com>
2020-12-23 11:56:37 +0200

    done

--- src/components/Dummy.js
+++ src/components/Dummy.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 export class Dummy extends Component {
     render(){
         return (<div>
+            <input type="text" placeholder="What do you think?"/>
+            <button>Post</button>
         </div>)
     }
 }
--- src/components/Spamalot.js
+++ src/components/Spamalot.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 
 
 class Spamalot extends Component {
-// your code here...   
+    render(){
+        const spamArr = []
+        for(let i=0; i<500; i++){
+            spamArr.push(<Spam />)
+        }
+        return (<div>{spamArr}</div>)
+    }
 }
 
 export default Spamalot
